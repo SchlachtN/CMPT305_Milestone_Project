aa27845b8baa6971870172fff5037b3811f9a0d8
Nolan Schlacht <supernolan2000@hotmail.ca>
2025-02-26 13:06:39 -0700

    Add documentation to Lab2Main. Refactor generalStats method.

--- src/main/java/ca/macewan/cmpt305/lab1/Lab2Main.java
+++ src/main/java/ca/macewan/cmpt305/lab1/Lab2Main.java
@@ -13,14 +13,17 @@ import java.util.Scanner;
  */
 public class Lab2Main {
     public static void main(String[] args) {
+        // Prompt user for file name (in resources directory)
         Scanner fileChoice = new Scanner(System.in);
         System.out.print("CSV filename: ");
         String fileName = fileChoice.nextLine();
         String filePath = "src/main/resources/" + fileName;
         PropertyAssessments propertyAssessments;
+        // Attempt to create PropertyAssessments from file and run menu
         try {
             propertyAssessments = new PropertyAssessments(filePath);
             dataMenu(propertyAssessments);
+        // Error location or reading file
         } catch (IOException e) {
             System.err.println("Error: can't open file: " + fileName);
         }
@@ -29,36 +32,39 @@ public class Lab2Main {
 
     /**
      * Display menu for user to select CSV data info.
-     * @param propertyAssessments - ca.macewan.cmpt305.lab1.PropertyAssessments class that stores ca.macewan.cmpt305.lab1.PropertyAssessments
+     * @param propertyAssessments - PropertyAssessments class that stores PropertyAssessments
      */
     public static void dataMenu(PropertyAssessments propertyAssessments) {
         boolean runMenu = true;
         String menu = menuDisplay();
         Scanner userSelection = new Scanner(System.in);
         while (runMenu) {
+            // Display menu and ask user for option selection
             System.out.print(menu);
             String choice = userSelection.nextLine();
             int choiceInt = checkValidInt(choice);
+            // User did not enter a number
             if (choiceInt == -1) {
                 System.out.println("Invalid input");
-            }
-            else if (choiceInt >= 1 && choiceInt <= 3) {
+            } else if (choiceInt >= 1 && choiceInt <= 3) { // Options 1-3
                 handleChoice(choiceInt, propertyAssessments);
-            }
-            else if (choiceInt == 4) {
+            } else if (choiceInt == 4) { // Select to close menu
                 runMenu = false;
-            } else {
+            } else { // Number option not on menu
                 System.out.println("Invalid choice");
             }
         }
     }
 
+    /**
+     * @return multi-line string for menu to display
+     */
     public static String menuDisplay() {
         return """
                 \nPlease select an option:
                 \t1) General Statistics
                 \t2) Individual Property Information
-                \t3) ca.macewan.cmpt305.lab1.Neighbourhood Information
+                \t3) Neighbourhood Information
                 \t4) Exit
                
                 Option:\s""";
@@ -70,13 +76,9 @@ public class Lab2Main {
      * @return choiceInt - Integer conversion of user choice or -1 if unable to parse to int
      */
     public static int checkValidInt(String choice) {
+        // Attempt to parse menu option and return
         try {
-            int choiceInt = Integer.parseInt(choice);
-            if (choiceInt == -1) {
-                return 0;
-            } else {
-                return choiceInt;
-            }
+            return Integer.parseInt(choice);
         } catch (NumberFormatException e) {
             return -1;
         }
@@ -85,17 +87,20 @@ public class Lab2Main {
     /**
      * Display data for the user depending on choice selection
      * @param choice - int menu choice by user
-     * @param propertyAssessments - ca.macewan.cmpt305.lab1.PropertyAssessments object containing ca.macewan.cmpt305.lab1.PropertyAssessment objects
+     * @param propertyAssessments - PropertyAssessments object containing PropertyAssessment objects
      */
     public static void handleChoice(int choice, PropertyAssessments propertyAssessments) {
         switch (choice) {
+            // General stats
             case 1:
                 System.out.println("\nDescriptive statistics of all property assessments");
                 generalStats(propertyAssessments);
                 break;
+            // Account info
             case 2:
                 propertyInfo(propertyAssessments);
                 break;
+            // Neighbourhood info
             case 3:
                 neighbourhoodStats(propertyAssessments);
                 break;
@@ -117,22 +122,16 @@ public class Lab2Main {
     }
 
     /**
-     * Call various methods in the ca.macewan.cmpt305.lab1.PropertyAssessments class to give general information
-     * @param propertyAssessments - ca.macewan.cmpt305.lab1.PropertyAssessments object containing list of ca.macewan.cmpt305.lab1.PropertyAssessment objects
+     * Call various methods in the PropertyAssessments class to give general information
+     * @param propertyAssessments - PropertyAssessments object containing list of PropertyAssessment objects
      */
     public static void generalStats(PropertyAssessments propertyAssessments) {
-        int n = propertyAssessments.getSize();
-        System.out.println("n = " + n);
-        int min = propertyAssessments.findMinimumValue();
-        System.out.println("min = " + currencyFormat(min));
-        int max = propertyAssessments.findMaximumValue();
-        System.out.println("max = " + currencyFormat(max));
-        int range = propertyAssessments.getRange();
-        System.out.println("range = " + currencyFormat(range));
-        int mean = propertyAssessments.getMean();
-        System.out.println("mean = " + currencyFormat(mean));
-        int median = propertyAssessments.getMedian();
-        System.out.println("median = " + currencyFormat(median));
+        System.out.println("n = " + propertyAssessments.getSize());
+        System.out.println("min = " + currencyFormat(propertyAssessments.findMinimumValue()));
+        System.out.println("max = " + currencyFormat(propertyAssessments.findMaximumValue()));
+        System.out.println("range = " + currencyFormat(propertyAssessments.getRange()));
+        System.out.println("mean = " + currencyFormat(propertyAssessments.getMean()));
+        System.out.println("median = " + currencyFormat(propertyAssessments.getMedian()));
     }
 
     /**
